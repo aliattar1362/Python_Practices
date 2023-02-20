b76a954deef60ab05d07ec9ea8086c66bedc0ae4
aliattar1362 <123591301+aliattar1362@users.noreply.github.com>
2023-02-21 00:08:02 +0200

    Update merkkijononkirjaimet.py

--- Strings/merkkijononkirjaimet.py
+++ Strings/merkkijononkirjaimet.py
@@ -1,21 +1,11 @@
  
 def get_input():
-    """
-    This is the main function. It calls several fucntions. 
-    First calls get_input fucntinos to make the list.
-    Then calls the functinos to calculate the desired 
-    statistical values.
-    """
+
     word = input("Enter a word: ")
     return word
 
 def word_seperator(word):
-    """
-    This is the main function. It calls several fucntions. 
-    First calls get_input fucntinos to make the list.
-    Then calls the functinos to calculate the desired 
-    statistical values.
-    """
+
     v_counter = 0
     for l in word:
         if l=="a"  or l=="e" or l=="i" or l=="o" or l== "u" or l== "y":
@@ -26,12 +16,7 @@ def word_seperator(word):
     return v_counter, c_counter
     
 def main():
-    """
-    This is the main function. It calls several fucntions. 
-    First calls get_input fucntinos to make the list.
-    Then calls the functinos to calculate the desired 
-    statistical values.
-    """
+
     word = get_input()
     v_counter, c_counter = word_seperator(word)
     print(f"The word \"{word}\" contains {v_counter} "
