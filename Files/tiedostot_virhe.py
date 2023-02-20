93c98f0184a860a293a4e51e4c797caf963bc8fc
aliattar1362 <123591301+aliattar1362@users.noreply.github.com>
2023-02-20 23:51:58 +0200

    Update tiedostot_virhe.py

--- Files/tiedostot_virhe.py
+++ Files/tiedostot_virhe.py
@@ -6,11 +6,7 @@ I learn about the error handling related to processing files.
 ############################################################        
         
 def main():
-    """
-    This function reads the number entered by the user,
-    checks that it is larger than zero, and returns it
-    to the caller of the function.
-    """
+
     filename = input("Enter the name of the file: ")
 
     try:
