461ad4245d5c6c8d819d9a8215558e2feebda102
aliattar1362 <123591301+aliattar1362@users.noreply.github.com>
2023-02-21 00:08:53 +0200

    Update Capitalization.py

--- Strings/Capitalization.py
+++ Strings/Capitalization.py
@@ -4,12 +4,7 @@ Getting acquainted with string methods in Python.
 Learning to use Python's documentation.
 """    
 def capitalize_initial_letters(text = "Aatu beetu CEETU Deetu eETU Feetu geetu Heetu IITU jiitu"):
-    """
-    This function uses a string as a parameter 
-    and returns it as written, with each word 
-    starting in upper case but the rest of the 
-    world in lower case.
-    """
+
     text = text.lower()
     text = text.split()
     #print("text:", text)
